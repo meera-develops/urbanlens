50fff4c56421657c5ec3a48efb312c54697c2ef6
Meera Bhola <meerabhola3@gmail.com>
2026-01-31 23:50:15 -0500

    refactored compare cards to add dropdown menus to each

--- src/pages/compare/compareCities.jsx
+++ src/pages/compare/compareCities.jsx
@@ -1,54 +1,113 @@
 import Cities from "../../components/citiesInfo.js";
-import { useParams } from "react-router-dom";
-import { Box, Typography, Grid, Button } from "@mui/material";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Box, Typography, Button } from "@mui/material";
 import { useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import CompareCard from "./comparisionCityCard.jsx";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 function compareCities() {
 
     const theme = useTheme();
-
+    const navigate = useNavigate();
     const { slug1, slug2 } = useParams();
-    const city1 = Cities.find(city => city.slug === slug1);
-    const city2 = Cities.find(city => city.slug === slug2);
+
+    const [option1, setOption1] = useState(slug1);
+    const [option2, setOption2] = useState(slug2);
+
+    const city1 = Cities.find(city => city.slug === option1);
+    const city2 = Cities.find(city => city.slug === option2);
+
     if (!city1 || !city2) {
         return <Typography variant="h2">City not found</Typography>
     }
 
+    const handleChange1 = (e) => {
+        const newSlug = e.target.value;
+        setOption1(newSlug);
+        navigate(`/compare/${newSlug}/${option2}`);
+    };
+
+    const handleChange2 = (e) => {
+        const newSlug = e.target.value;
+        setOption2(newSlug);
+        navigate(`/compare/${option1}/${newSlug}`);
+    };
+
     return (
         <>
-
             <Box sx={{
                 display: 'flex',
-                flexDirection: {xs: 'column', sm: 'row',},
+                flexDirection: {xs: 'column', sm: 'row'},
                 justifyContent: 'center',
-                alignItems: {xs: 'center', md: 'flex-start'},
+                alignItems: {xs: 'center', sm: 'flex-start'},
                 gap: {xs: 4, md: 0},
                 pt: 3,
             }}>
-                <CompareCard
-                    title={city1.title}
-                    qol={city1.qol}
-                    image={city1.img}
-                    population={city1.population}
-                    climate={city1.climate}
-                    price={city1.price}
-                    commute={city1.avg_commute}
-                    cost_living={city1.avg_cost_of_living}
-                    slug={city1.slug}
-                />
-                <CompareCard
-                    title={city2.title}
-                    qol={city2.qol}
-                    image={city2.img}
-                    population={city2.population}
-                    climate={city2.climate}
-                    price={city2.price}
-                    commute={city2.avg_commute}
-                    cost_living={city2.avg_cost_of_living}
-                    slug={city2.slug}
-                />
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mr: {md: 4, xl: 10,} }}>
+                    <FormControl variant="filled" sx={{ width: '200px', mb: 2 }}>
+                        <InputLabel>City 1</InputLabel>
+                        <Select
+                          value={option1}
+                          label="City 1"
+                          onChange={handleChange1}
+                          sx={{ backgroundColor: 'rgba(120, 145, 116, 0.5)' }}
+                        >
+                          {Cities.map((c) => (
+                            <MenuItem key={c.slug} value={c.slug}
+                            sx={{ fontFamily: "'Pontano Sans', serif" }}
+                            >
+                              {c.title}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                    </FormControl>
+                    <CompareCard
+                        title={city1.title}
+                        qol={city1.qol}
+                        image={city1.img}
+                        population={city1.population}
+                        climate={city1.climate}
+                        price={city1.price}
+                        commute={city1.avg_commute}
+                        cost_living={city1.avg_cost_of_living}
+                        slug={city1.slug}
+                    />
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <FormControl variant="filled" sx={{ width: '200px', mb: 2 }}>
+                        <InputLabel>City 2</InputLabel>
+                        <Select
+                          value={option2}
+                          label="City 2"
+                          onChange={handleChange2}
+                          sx={{ backgroundColor: 'rgba(120, 145, 116, 0.5)' }}
+                        >
+                          {Cities.map((c) => (
+                            <MenuItem key={c.slug} value={c.slug}
+                            sx={{ fontFamily: "'Pontano Sans', serif" }}
+                            >
+                              {c.title}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                    </FormControl>
+                    <CompareCard
+                        title={city2.title}
+                        qol={city2.qol}
+                        image={city2.img}
+                        population={city2.population}
+                        climate={city2.climate}
+                        price={city2.price}
+                        commute={city2.avg_commute}
+                        cost_living={city2.avg_cost_of_living}
+                        slug={city2.slug}
+                    />
+                </Box>
             </Box>
             <Box
             sx={{
